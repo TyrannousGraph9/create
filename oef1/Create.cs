c7f78f88d543690459ff8b300eef4c7be68121f1
TyrannousGraph9 <2405594cremers@zuyd.nl>
2024-12-02 13:50:48 +0100

    things for fun

--- oef1/Create.cs
+++ oef1/Create.cs
@@ -9,14 +9,6 @@ namespace Create
             Console.WriteLine("Wat is je naam?");
             string naam = Console.ReadLine();
             Log("Naam: " + naam);
-            Car car = new Car();
-            Console.WriteLine("Wat is het merk van je auto?");
-            car.brand = Console.ReadLine();
-            Log("Merk: " + car.brand);
-            Console.WriteLine("Wat is het model van je auto?");
-            car.model = Console.ReadLine();
-            Log("Model: " + car.model);
-            Read();
         }
 
         public void Log(string message)
@@ -33,6 +25,7 @@ namespace Create
             {
                 Console.WriteLine(line);
             }
+            File.Delete(filePath);
         }
     }
     class Car
--- oef1/Program.cs
+++ oef1/Program.cs
@@ -8,6 +8,13 @@ namespace Create
         {
             Myprogram myprogram = new Myprogram();
             myprogram.Main();
+            Car car = new Car();
+            Console.WriteLine("Wat is het merk van je auto?");
+            car.brand = Console.ReadLine();
+            myprogram.Log("Merk: " + car.brand);
+            Console.WriteLine("Wat is het model van je auto?");
+            car.model = Console.ReadLine();
+            myprogram.Read();
         }
     }
 }
