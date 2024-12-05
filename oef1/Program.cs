09bf7a0e2d138e76a6af92c557ce2119dabba1ac
TyrannousGraph9 <2405594cremers@zuyd.nl>
2024-12-05 11:20:18 +0100

    opdracht 5 en bugfixes

--- oef1/Program.cs
+++ oef1/Program.cs
@@ -14,6 +14,14 @@ namespace Create
         
             Console.WriteLine("Opdracht 3");
             Opdracht_3();
+
+            Console.WriteLine("Opdracht 4");
+            int uitkomst = 0;
+            Opdracht_4(uitkomst);
+
+            Console.WriteLine("Opdracht 5");
+            string uitkomst_2 = "";
+            Opdracht_5(uitkomst_2);
         }
 
         static void Opdracht_1()
@@ -27,6 +35,10 @@ namespace Create
             {
                 Console.WriteLine("Je bent volwassen.");
             }
+            else if(leeftijd > 18)
+            {
+                Console.WriteLine("Je bent meerderjarig.");
+            }
             else
             {
                 Console.WriteLine("Je bent minderjarig.");
@@ -38,19 +50,22 @@ namespace Create
             Console.WriteLine("Hoevaak wil je een tekst hebben herhaald?");
 
             int aantal = Convert.ToInt32(Console.ReadLine());
+            int max_aantal = aantal;
 
             for (int i = 0; i < aantal; i++)
             {
             Console.WriteLine("Dit is regel " + (i + 1));
             }
 
+            aantal = max_aantal;
             do
             {
             Console.WriteLine("Dit is regel " + aantal);
             aantal--;
             } while (aantal > 0);
 
-            aantal = Convert.ToInt32(Console.ReadLine());
+            aantal = max_aantal;
+
             while (aantal > 0)
             {
             Console.WriteLine("Dit is regel " + aantal);
@@ -80,6 +95,11 @@ namespace Create
                     Console.WriteLine(numb1 - numb2);
                     break;
                 case "*":
+                    if(numb1 == 0 || numb2 == 0)
+                    {
+                        Console.WriteLine("0");
+                    }
+                    else
                     Console.WriteLine(numb1 * numb2);
                     break;
                 case "/":
@@ -90,7 +110,7 @@ namespace Create
                     break;
             }
         }
-        void Opdracht_4(int uitkomst)
+        static void Opdracht_4(int uitkomst)
         {
             Console.WriteLine("Geef een getal in");
             int numb1 = Convert.ToInt32(Console.ReadLine());
@@ -115,6 +135,11 @@ namespace Create
                     uitkomst = numb1 - numb2;
                     break;
                 case "*":
+                    if(numb1 == 0 || numb2 == 0)
+                    {
+                        Console.WriteLine("0");
+                    }
+                    else
                     uitkomst = numb1 * numb2;
                     break;
                 case "/":
@@ -132,7 +157,25 @@ namespace Create
             Console.WriteLine("Het antwoord is: " + uitkomst);
         }
 
-        
+        static void Opdracht_5(string uitkomst)
+        {
+            Console.WriteLine("Geef een getal in");
+            int numb1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geef nog een getal in");
+            int numb2 = Convert.ToInt32(Console.ReadLine());
+            
+            uitkomst = Calculate_3(numb1, numb2);
+            Console.WriteLine(uitkomst);
+        }
+
+        public static string Calculate_3(int numb1, int numb2)
+        {
+            string uitkomst = "";
+            uitkomst = "Als ik deze getallen optel krijg ik: " + (numb1 + numb2);
+            return uitkomst;
+        }
+
+
 
 
 
