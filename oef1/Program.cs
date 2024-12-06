dfdf8bca6cb80e1bb2d597dab30758c1b3d5d815
TyrannousGraph9 <2405594cremers@zuyd.nl>
2024-12-06 11:33:47 +0100

    all done

--- oef1/Program.cs
+++ oef1/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.IO;
 namespace Create
 {
     public class Opdrachten
     {
         static void Main()
         {
+    
             Console.WriteLine("Opdracht 1");
             Opdracht_1();
 
@@ -22,6 +23,15 @@ namespace Create
             Console.WriteLine("Opdracht 5");
             string uitkomst_2 = "";
             Opdracht_5(uitkomst_2);
+
+            Console.WriteLine("Opdracht 6");
+            Opdracht_6();
+
+            Console.WriteLine("Opdracht 7");
+            Opdracht_7();
+
+            Console.WriteLine("Opdracht 8");
+            Opdracht_8();
         }
 
         static void Opdracht_1()
@@ -175,11 +185,140 @@ namespace Create
             return uitkomst;
         }
 
+        static void Opdracht_6()
+        {
+            List<int> list = new List<int>();
+            
+            for (int i = 1; i < 6; i++)
+            {
+                list.Add(i);
+            }
+
+            Console.WriteLine("Welk getal wil je verwijderen?");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            if(numb > 6 || numb < 1)
+            {
+                Console.WriteLine("Dit getal staat niet in de lijst.");
+                return;
+            }
+            else
+            {
+                list.Remove(numb);
+                Console.WriteLine("Het getal is verwijderd, dit blijft er over:");
+                foreach (int i in list)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void Opdracht_7()
+        {
+            int[] numbArray = [1,2,3,4,5,6];
+            
+            Console.WriteLine("Welk getal wil je verwijderen?");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            if(numb > 6 || numb < 1)
+            {
+                Console.WriteLine("Dit getal staat niet in de lijst.");
+                return;
+            }
+            else
+            {
+                int[] newArray = numbArray.Take(numb).ToArray();
+                newArray = numbArray.Where(x => x != numb).ToArray();
+                Console.WriteLine("Het getal is verwijderd, dit blijft er over:");
+                foreach (int i in newArray)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void MaakTekstBestand()
+        {
+            File.Create("namen.txt");
+    
+            string[] names = {"Jan", "Piet", "Joris", "Korneel"};
+            File.WriteAllLines("namen.txt", names);
+        }
+        static void Opdracht_8()
+        {
+            MaakTekstBestand();
+            Console.WriteLine("Wat wil je doen? (add, remove, display)");
+            string keuze = Console.ReadLine();
+
+            switch(keuze)
+            {
+                case "add":
+                    Add();
+                    break;
+                case "remove":
+                    Remove();
+                    break;
+                case "display":
+                    Display();
+                    break;
+                default:
+                    Console.WriteLine("Ongeldige keuze");
+                    Opdracht_8();
+                    break;
+            }
+        }
 
 
+        static void Add()
+        {
+            Console.WriteLine("Welke naam wil je toevoegen?");
+            string naam = Console.ReadLine();
+            string fileContents = File.ReadAllText("namen.txt");
+            if(fileContents.Contains(naam))
+            {
+                Console.WriteLine("Deze naam staat al in het bestand");
+                return;
+            }
+            else
+            {
+                File.Open("namen.txt", FileMode.Append);
 
+                File.AppendAllText("namen.txt", naam + Environment.NewLine);
+                Console.WriteLine("Naam toegevoegd");
+                Opdracht_8();
+            }
+        }
 
+        static void Remove()
+        {
+            Console.WriteLine("Welke naam wil je verwijderen?");
+            string naam = Console.ReadLine();
+            string fileContents = File.ReadAllText("namen.txt");
+            if(fileContents.Contains(naam))
+            {
+                fileContents = fileContents.Replace(naam, "");
+                File.WriteAllText("namen.txt", fileContents);
+                Console.WriteLine("Naam verwijderd");
+            }
+            else
+            {
+                Console.WriteLine("Naam niet gevonden");
+            }
 
+        }
 
+        static void Display()
+        {
+            Console.WriteLine("Welke naam wil je zien?");
+            string naam = Console.ReadLine();   
+            string fileContents = File.ReadAllText("namen.txt");
+            if(fileContents.Contains(naam))
+            {
+                Console.WriteLine(naam);
+                Opdracht_8();
+            }
+            else
+            {
+                Console.WriteLine("Naam niet gevonden");
+            }
+        }
     }
 }
